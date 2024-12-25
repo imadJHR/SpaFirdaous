ad592e11931da3364753343409bdba8afc2ea838
imadJHR <143011603+imadJHR@users.noreply.github.com>
2024-12-25 05:14:53 +0100

    Update services.js

--- src/data/services.js
+++ src/data/services.js
@@ -1,198 +1,75 @@
-import Keratine from "../assets/keratine.jpg"
+import Keratine from "../assets/keratine.jpg";
 
 export const services = [
   {
-      title: "Hammam Au Chocolat",
-      description: "Gommage + le gant + tabrima sahrawia + savonnage au chocolat.",
-      duration: "60 min",
-      price: "200",
-      image: "https://marajahspa.com/wp-content/uploads/2023/01/PHOTO-2023-01-11-17-10-048-1024x682.jpg"
-    },
- {
-      title: "Hammam Anti-Stress",
-      description: "Gommage + le gant + tabrima + savonnage A base de pamplemousse + envolopement mask antifatique + brushing.",
-      duration: "60 min",
-      price: "300",
-      image: "https://www.bienetreconfort.fr/wp-content/uploads/2018/05/hammam-anti-stress.jpg"
-    },
-  {
-      title: "Pack Hammam Aux Algues Medicales",
-      description: "Mask algue avec argile verte (anti taches et acné) + gommage au gant + un petit gommage au sel marin (pour améliorer la circulation du sang) + envolopement 7 plantes chauffantes (relaxation musculaire) + soin cheveux bonnet + mask visage nila et savonage avec un gel (anti fatigue) + 1 Heure massage médical et brushing.",
-      duration: "75 min",
-      price: "480",
-      image: "https://lesbainsdeazahara.net/wp-content/uploads/2022/05/P1000643-scaled.jpg"
-    },
-  {
-      title: "Pack Hammam VIP",
-      description: "Gommage + Savonnage + 1 Heure massage + Brushing.",
-      duration: "90 min",
-      price: "250",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8tsK7RlO0dOtNoNVXCg2Julkd4QQuogwELJeBOBpgienV8tB2e3bSA0I9vH48eiTz4ig&usqp=CAU"
-    },
- {
-      title: "Pack Hammam Shahrazade",
-      description: "Gommage + Savonnage + La Cire Complette + Brushing.",
-      duration: "60 min",
-      price: "299",
-      image: "https://img.freepik.com/photos-gratuite/cadre-spa-bougie-allumee-serviettes-moelleuses-fleurs-parfumees-favorise-relaxation_91128-4634.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid"
-    },
-  {
-      title: "Pack Sultana",
-      description: "Gommage + Savonnage + Pose Vernie Permanant + Brushing.",
-      duration: "60 min",
-      price: "250",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_bnaQ7vWDnoY8_G7GP4Kpnvhg3vuaDlkmS6-TEUK1NwTyjd8qlKcz1p28rkzrkmsxIFA&usqp=CAU"
-    },
-    
-    {
-      title: "Pack Hammam Loock",
-      description: "Gommage + Savonnage + Coloration Des Cheveux  + Brushing.",
-      duration: "60 min",
-      price: "350",
-      image: "https://img.freepik.com/photos-gratuite/jolie-femme-africaine-beneficiant-massage-du-visage-dans-salon-spa_176420-13955.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid"
-    },
-    {
-      title: "Pack Hammam Royal",
-      description: "Gommage + Savonnage + Manucure Spa + Pedicure Spa + Massage 30 min + Brushing.",
-      duration: "60 min",
-      price: "350",
-      image: "https://img.freepik.com/photos-gratuite/equipements-traitement-spa-base-plantes-mis-plancher-bois_1150-34506.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid"
-    },
-    {
-      title: "Pack Hammam Luxe ",
-      description: "Gommage + Savonnage + Soin De Visage Hydratacial  + Brushing.",
-      duration: "60 min",
-      price: "400",
-      image: "https://img.freepik.com/photos-gratuite/gros-plan-main-therapeute-masser-dos-femme-serviette-chaude-dans-spa_23-2147939902.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid"
-    },
-    {
-      title: "Pack Hammam Prestige",
-      description: "Gommage + Savonnage + Manucure Spa + Pedicure Spa + Brushing.",
-      duration: "60 min",
-      price: "300",
-      image: "https://img.freepik.com/photos-gratuite/femme-se-detendre-dans-chambre-hotel_23-2148095308.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid"
-    },
-   {
-      title: "Head Spa Basique",
-      description: "Head Spa 45 min + Shampoin + Demeleur.",
-      duration: "60 min",
-      price: "320",
-      image: "https://img.freepik.com/photos-gratuite/produits-spa_144627-5520.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid"
-    },
-    {
-      title: "Head Spa Antichute",
-      description: "Head Spa 1 Heure + Soin Cheveux Antichute.",
-      duration: "60 min",
-      price: "380",
-      image: "https://www.graindebeaute-merville.fr/wp-content/uploads/2024/03/japanese-head-spa-sliedrecht-slider-1280x800.jpg"
-    },
-    {
-      title: "Head Spa Soin Hydratant",
-      description: "Head Spa 1 Heure + Soin Cheveux Hydratant.",
-      duration: "60 min",
-      price: "380",
-      image: "https://img.freepik.com/photos-gratuite/compresse-herbes-equipements-traitement-spa-base-plantes-mis-sol-sombre_1150-34493.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid"
-    },
-    {
-      title: "Head Spa Antipelliculair",
-      description: "Head Spa 1 Heure + Soin Cheveux Pelliculaire.",
-      duration: "60 min",
-      price: "380",
-      image: "https://img.freepik.com/photos-gratuite/traitement-aromatherapie-bougies_144627-20318.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid"
-    },
-  {
-      title: "Pack Paradise Anti-Stress",
-      description: "Gommage + le gant + tabrima + savonnage A base de pamplemousse + envolopement mask antifatique + brushing + 1 Heure massage anti-stress.",
-      duration: "90 min",
-      price: "350",
-      image: "https://www.bienetreconfort.fr/wp-content/uploads/2018/02/hammam-bienfaits-corps-esprit.jpg"
-    },
-  {
-      title: "Pack Hammam Royal",
-      description: "Gommage + Savonnage + Entré Mariée + Masque Corporel Laroussa + 1 Heure De Massage Anti Stress.",
-      duration: "60 min",
-      price: "1000",
-      image: "https://img.freepik.com/photos-gratuite/elements-naturels-pour-spa-bougies_23-2148199525.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid"
-    },
-    {
-      title: "Pack Mariée",
-      description: "Entree Mariée Avec Les Accessoires + M'aquige Professionnel (Mariée) + Chignon + Epilation Complete + Manucure Royal + Peducure Royal + Gel Mains Et Pieds , Soin Hydra Facial Royal.",
-      duration: "60 min",
-      price: "3000",
-      image: "https://img.freepik.com/photos-gratuite/jeune-femme-sentir-herbe-fleurs-debout-dans-sauna_23-2147867889.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid"
-    },
- {
-    title: "Microneedling",
-    description: "Vivez une relaxation profonde avec nos Microneedling",
-    image:
-      "https://myla-bella.com/wp-content/uploads/2024/09/microneedling.jpg",
-    price: "400",
-    duration: "1 séance",
-  },
-  {
-    title: "Soins du visage Royal",
-    description: "Revitalisez votre peau avec des ingrédients naturels",
-    image:
-      "https://www.mademoisellerelax.fr/wp-content/uploads/2018/06/mademoiselle-relax-soins-visage-2.jpg",
-    price: "400",
+    title: "Hammam Au Chocolat Relaxant et Gourmand",
+    description: "Découvrez un soin relaxant avec gommage, gant traditionnel, tabrima sahrawia et savonnage au chocolat pour une peau douce et nourrie.",
     duration: "60 min",
+    price: "200",
+    image: "https://marajahspa.com/wp-content/uploads/2023/01/PHOTO-2023-01-11-17-10-048-1024x682.jpg",
   },
-   
-    
-   
-   
-   
-   
-    {
-      title: "Pédicure Royal",
-      description: "Gommage Anti Stress + Mask Anti Stress + Paraffine + Massage Bougie.",
-      duration: "60 min",
-      price: "150",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN9-SOqThd-S4Naf6v13VS0RcyWArweOXkIw&s"
-    },
-   
-  
-    {
-      title: "Blanchiment dentaire",
-      description: "Ravivez l'éclat naturel de votre sourire avec un soin professionnel et sécurisé.",
-      duration: "3 séances",
-      price: "600",
-      image: "https://www.drmorganecohensamama.com/wp-content/uploads/2019/12/blanchiment-dr-morgane-cohen-chirurgien-dentiste.jpg"
-    },
-    {
-      title: "Lifting Colombien",
-      description: "",
-      duration: "1 séance",
-      price: "150",
-      image: "https://universequipementmedical.com/fr/wp-content/uploads/2022/01/19aff6fa1f9d8c6_big_big.jpg"
-    },
   {
-      title: "Glaçage des Levres",
-      description: "",
-      duration: "2 séances",
-      price: "1000",
-      image: "https://i0.wp.com/epilmacmorocco.com/wp-content/uploads/2021/02/maquillage-permanent-rabat.jpg?w=1000&ssl=1"
-    },
-   {
-      title: "Protein et Keratine",
-      description: "Toutes les longeurs",
-      duration: "90 min",
-      price: "799",
-      image: Keratine
-    },
-   {
-      title: "Stickers Extensions",
-      description: "40 Bandes",
-      duration: "90 min",
-      price: "1350",
-      image: "https://studio-extensions.com/wp-content/uploads/2019/03/TAPES3.jpg"
-    },
-   {
-      title: "Cil Permanent",
-      description: "100% en sole",
-      duration: "60 min",
-      price: "400",
-      image: "https://socabeautysophia.com/cdn/shop/files/Screenshot_20220418-112112_Instagram.jpg?v=1653041834&width=1920"
-    },
-  ];
-  
+    title: "Hammam Anti-Stress pour la Relaxation",
+    description: "Gommage avec gant, savonnage au pamplemousse, enveloppement antifatigue, brushing et soins apaisants pour réduire le stress.",
+    duration: "60 min",
+    price: "300",
+    image: "https://www.bienetreconfort.fr/wp-content/uploads/2018/05/hammam-anti-stress.jpg",
+  },
+  {
+    title: "Pack Hammam Aux Algues Médicales Purifiantes",
+    description: "Soin complet avec masque aux algues et argile verte, gommage au sel marin, enveloppement relaxant et massage médical.",
+    duration: "75 min",
+    price: "480",
+    image: "https://lesbainsdeazahara.net/wp-content/uploads/2022/05/P1000643-scaled.jpg",
+  },
+  {
+    title: "Pack Hammam VIP Spa Deluxe",
+    description: "Offrez-vous un moment de luxe avec gommage, savonnage, massage d'une heure et brushing pour une détente totale.",
+    duration: "90 min",
+    price: "250",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8tsK7RlO0dOtNoNVXCg2Julkd4QQuogwELJeBOBpgienV8tB2e3bSA0I9vH48eiTz4ig&usqp=CAU",
+  },
+  {
+    title: "Pack Hammam Shahrazade Oriental",
+    description: "Une expérience orientale complète avec gommage, savonnage, épilation à la cire et brushing.",
+    duration: "60 min",
+    price: "299",
+    image: "https://img.freepik.com/photos-gratuite/cadre-spa-bougie-allumee-serviettes-moelleuses-fleurs-parfumees-favorise-relaxation_91128-4634.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid",
+  },
+  {
+    title: "Pack Sultana Beauté Royale",
+    description: "Soin beauté complet avec gommage, savonnage, pose de vernis permanent et brushing pour une allure royale.",
+    duration: "60 min",
+    price: "250",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_bnaQ7vWDnoY8_G7GP4Kpnvhg3vuaDlkmS6-TEUK1NwTyjd8qlKcz1p28rkzrkmsxIFA&usqp=CAU",
+  },
+  {
+    title: "Pack Hammam Royal Luxe",
+    description: "Gommage, savonnage, manucure spa, pédicure spa, massage de 30 minutes et brushing pour une détente absolue.",
+    duration: "60 min",
+    price: "350",
+    image: "https://img.freepik.com/photos-gratuite/equipements-traitement-spa-base-plantes-mis-plancher-bois_1150-34506.jpg?uid=R115960642&ga=GA1.1.519463234.1718278238&semt=ais_hybrid",
+  },
+  {
+    title: "Protein et Keratine pour Cheveux Sublimés",
+    description: "Soin profond pour toutes les longueurs, redonnez brillance et douceur à vos cheveux avec nos traitements à la kératine.",
+    duration: "90 min",
+    price: "799",
+    image: Keratine,
+  },
+  {
+    title: "Microneedling Réparateur pour une Peau Lisse",
+    description: "Profitez des bienfaits du microneedling pour rajeunir votre peau et réduire les imperfections.",
+    duration: "1 séance",
+    price: "400",
+    image: "https://myla-bella.com/wp-content/uploads/2024/09/microneedling.jpg",
+  },
+  {
+    title: "Blanchiment Dentaire Professionnel",
+    description: "Redonnez éclat et blancheur à vos dents grâce à notre soin sécurisé de blanchiment dentaire.",
+    duration: "3 séances",
+    price: "600",
+    image: "https://www.drmorganecohensamama.com/wp-content/uploads/2019/12/blanchiment-dr-morgane-cohen-chirurgien-dentiste.jpg",
+  },
+  // Ajoutez ici les autres éléments optimisés pour le SEO
+];
